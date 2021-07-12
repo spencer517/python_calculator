c0c28c929f1247922a37463381abaf2e960c8cb1
Spencer <spencer517@msn.com>
2021-07-12 15:07:50 -0400

    attempted filepath fix

--- src/testCalculator.py
+++ src/testCalculator.py
@@ -12,17 +12,17 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.calculator, Calculator)
 
     def test_csv_add(self):
-        reader = CsvReader('/src/Addition.csv').data
+        reader = CsvReader('./src/Addition.csv').data
         for row in reader:
             self.assertEqual(self.calculator.add(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
 
     def test_csv_subtract(self):
-        reader = CsvReader('/src/Subtraction.csv').data
+        reader = CsvReader('./src/Subtraction.csv').data
         for row in reader:
             self.assertEqual(self.calculator.subtract(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
 
     def test_csv_multiply(self):
-        reader = CsvReader('/src/Multiplication.csv').data
+        reader = CsvReader('./src/Multiplication.csv').data
         for row in reader:
             self.assertEqual(self.calculator.multiply(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
 
@@ -32,12 +32,12 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.divide(float(row['Value 1']), float(row['Value 2'])), round(float(row['Result']),6))
 
     def test_csv_square(self):
-        reader = CsvReader('/src/Square.csv').data
+        reader = CsvReader('./src/Square.csv').data
         for row in reader:
             self.assertEqual(self.calculator.square(int(row['Value 1'])),  int(row['Result']))
 
     def test_csv_squareroot(self):
-        reader = CsvReader('/src/SquareRoot.csv').data
+        reader = CsvReader('./src/SquareRoot.csv').data
         for row in reader:
             self.assertEqual(self.calculator.squareroot(int(row['Value 1'])),  round(float(row['Result']),3))
 
