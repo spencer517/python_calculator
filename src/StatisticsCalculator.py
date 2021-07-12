e9fe6fcd0af55926e655209ffaaa1979457f0210
Spencer <spencer517@msn.com>
2021-07-11 20:56:54 -0400

    Initial Commit for Statistics Calculator project

---
+++ src/StatisticsCalculator.py
@@ -0,0 +1,23 @@
+from Calculator import Calculator
+
+
+class StatisticsCalculator(Calculator):
+
+    def __init__(self):
+        pass
+
+    def mean(numbers):
+        pass
+
+    def median(self):
+        pass
+
+    def mode(self):
+        pass
+
+    def stddev(self):
+        pass
+
+    def varicance(self):
+        pass
+
